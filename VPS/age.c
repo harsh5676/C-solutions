b0c686343e1e1c122367ed45fd1b627bc52ca24c
harsh5676 <70193045+harsh5676@users.noreply.github.com>
2021-10-19 16:23:41 +0530

    19-10-2021

---
+++ VPS/age.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int age;
+    char gender;
+    char name[15];
+    printf("What is your age?\n");
+    scanf("%d", &age);
+    printf("What is your gender?(m/f)\n");
+    scanf("%c", &gender);
+    getchar();
+    printf("What is your name?\n");
+    scanf("%s", &name);
+
+    if (age >= 18)
+    {
+        printf("You are eligible.\n");
+
+        if (gender == 'm')
+        {
+            printf("Hey dude you are %d years old and your name is %s.\n", age, name);
+        }
+        else if (gender == 'f')
+        {
+            printf("Hey lady you are %d years old and your name is %s.\n", age, name);
+        }
+        else
+        {
+            printf("Invalid Choice!\n");
+        }
+    }
+    else
+    {
+        printf("You are not eligible.\n");
+    }
+
+    getch();
+    return 0;
+}
